11a0c0bd1bf7ccd6a61bb484bda63698099dfe0e
Радмир Карманов <karr@soft-servis.local>
2023-04-19 10:35:48 +0500

    add button in auth.tsx

--- app/components/ui/button/Button.tsx
+++ app/components/ui/button/Button.tsx
@@ -11,17 +11,21 @@ const Button: FC<PropsWithChildren<IButton>> = ({
   children,
   ...rest
 }) => {
+  const icon = <Icon name='video' size={30} />
+
   return (
     <View>
       <Pressable className={cn('self-center mt-3.5', className)} {...rest}>
         <LinearGradient
           start={{ x: 0, y: 0.75 }}
           end={{ x: 1, y: 0.25 }}
-          className='w-full py-3 px-8 rounded-2xl items-center'
+          className={cn('w-full py-3 px-8 rounded-2xl items-center', {
+            'flex-row': !!icon
+          })}
           colors={['#DC3F41', '#a6282b']}
         >
-          <Text className='text-white text-center font-medium text-lg'>
-            <Icon name='video' size={30} />
+          {icon && <Icon name='video' color='white' size={20} />}
+          <Text className='text-white text-center font-medium text-lg ml-2'>
             {children}
           </Text>
         </LinearGradient>
